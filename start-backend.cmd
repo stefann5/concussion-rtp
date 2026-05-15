67cec25655852d5d386c59ccd152ca3adbfa325f
Stefan Nikolić <stefan.nik04@gmail.com>
2026-05-15 19:30:21 +0200

    fix start backend script

--- start-backend.cmd
+++ start-backend.cmd
@@ -3,18 +3,12 @@ setlocal
 
 cd /d "%~dp0"
 
-echo === Installing parent pom ===
-call mvn -N install -q || goto :err
-
-echo === Building model ===
-call mvn -pl model install -DskipTests -q || goto :err
-
-echo === Building kjar ===
-call mvn -pl kjar install -DskipTests -q || goto :err
+echo === Clean install (model + kjar) ===
+call mvn -pl model,kjar -am clean install -DskipTests -q || goto :err
 
 echo === Starting service on http://localhost:8080 ===
 cd service
-call mvn spring-boot:run
+call mvn clean spring-boot:run
 
 goto :eof
 
